3a710dba24474d6d863458d559103d2e89aee9e4
shane <shanestevensxk9@gmail.com>
2022-06-07 10:19:50 +0200

    update

---
+++ code.js
@@ -0,0 +1,5 @@
+let bestFriends = ["Josh","Aiden","Jack","James","Ryan","John","Reagan","Monique","Marshalino","Shane"];
+
+console.table(bestFriends);
+console.log(bestFriends[bestFriends.length/2]);
+console.log(bestFriends[bestFriends.length-1]);
